d596ed063adb942b436f704b0a7a903848b144b3
maruf5682 <mdmarufur5682@gmail.com>
2023-07-15 16:03:49 +0600

    scroll top

--- js/index.js
+++ js/index.js
@@ -6,3 +6,28 @@ navbars.forEach(e=>{
         this.classList.add('active')
     })
 })
+
+
+
+
+// back to top up 
+
+// Get the button
+let myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
